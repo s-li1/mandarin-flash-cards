070df000b79f05ea58a5df5913caee2ae1ed125c
Steven.J.Li <14027496@student.uts.edu.au>
2020-11-15 22:55:49 +1100

    Removed remains of reportWebVitals and converted DrawButton class component to function component

--- src/DrawButton/DrawButton.js
+++ src/DrawButton/DrawButton.js
@@ -1,25 +1,12 @@
-import React, { Component } from 'react';
-import './DrawButton.css';
+import React from 'react';
 
-class DrawButton extends Component{
-    constructor(props){
-        super(props);
-        //necessary binding to make 'this' work in call back
-        this.drawCard = this.drawCard.bind(this);
+export default function DrawButton(props) {
+    function drawCard() {
+        props.drawCard();
     }
-
-    drawCard(){
-        this.props.drawCard();
-    }
-
-    render(props){
-        return(
-            <div className="buttonContainer">
-                <button className="btn" onClick={this.drawCard}>Draw Card</button>
-            </div>
-        )
-    }
+    return(
+        <div className="buttonContainer">
+            <button className="btn" onClick={drawCard}>Draw Card</button>
+        </div>
+    )
 }
-
-export default DrawButton
-
